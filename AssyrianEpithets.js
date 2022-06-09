bffb481ca057f39fca8563c2adc511405400912b
matthewjparkes <78561107+matthewjparkes@users.noreply.github.com>
2022-06-09 22:32:30 +0100

    Added Message Arrays

--- AssyrianEpithets.js
+++ AssyrianEpithets.js
@@ -1,1 +1,3 @@
-const test = 0;
+const king_name = ["Sennacherib", "Sargon", "Ashurbanipal", "Esarhaddon", "Ashurnasirpal", "Shalmaneser", "Tiglath-Pileser"];
+const epithet_1 = ["foremost of all rulers", "the one who strikes enemies with lightning", "guardian of truth who loves justice", "great king", "strong king", "virile warrior", "favorite of the great gods", "the true shepherd who reorganized the confused people", "renewed the statues of the great gods", "beloved of the goddess Zarpanītu", "beloved of the god Marduk"];
+const location = ["king of the world", "king of Assyria", "king of Sumer and Akkad", "king of the four quarters", "governor of Babylon"];
